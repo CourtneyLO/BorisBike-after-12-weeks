e388340fafaa3985be6ab82b99b530c5e16c76c2
Courtney Osborn <courtney.lee.osborn@gmail.com>
2017-02-20 20:40:09 +0000

    Adds broken bikes to fix_bikes array

--- lib/garage.rb
+++ lib/garage.rb
@@ -1,9 +1,13 @@
 class Garage
 
   attr_reader :fix_bikes
-  
+
   def initialize
     @fix_bikes = []
+  end
+
+  def accept_bike(bike)
+    @fix_bikes.push(bike)
   end
 
 end
--- spec/garage_spec.rb
+++ spec/garage_spec.rb
@@ -4,7 +4,14 @@ describe Garage do
 
   subject(:garage) { described_class.new }
 
+  let(:bike) { double :bike }
+
   it("should initialize with an empty fix_bikes array") do
     expect(garage.fix_bikes).to eq([])
+  end
+
+  it("should accept bikes") do
+    garage.accept_bike(bike)
+    expect(garage.fix_bikes).to include(bike)
   end
 end
